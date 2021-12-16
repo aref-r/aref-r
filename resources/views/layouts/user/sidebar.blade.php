e94537a9d2f3d04d87cb69cc59c64b08d08fb0e6
web flax <web.flax.co@gmail.com>
2021-12-16 17:18:03 +0330

    'make_some_changes_on_commentfortickets'

--- resources/views/layouts/user/sidebar.blade.php
+++ resources/views/layouts/user/sidebar.blade.php
@@ -1,29 +1,29 @@
 @if (Auth::user()->lang == 'en')
-<div class="sidebar">
-    <a class="brand-logo" href="index.html">
-        <img src="{{asset('/images/logo.png')}}" alt="">
-        <span>Pay Pooler </span></a>
-    <div class="menu">
-        <ul>
-            <li>
-                <a href="/home">
-                    <span><i class="mdi mdi-view-dashboard"></i></span>
-                    <span class="nav-text">Home</span>
-                </a>
-            </li>
-            <li><a href="{{ route('user.order.index') }}">
-                    <span><i class="mdi mdi-repeat"></i></span>
-                    <span class="nav-text">My Orders</span>
-                </a>
-            </li>
-            <li><a href="{{ route('user.offer.index') }}">
-                    <span><i class="mdi mdi-repeat"></i></span>
-                    <span class="nav-text">My Offers</span>
-                </a>
-            </li>
-            <li><a href="accounts.html">
-                    <span><i class="mdi mdi-account"></i></span>
-                    <span class="nav-text">Account</span>
+    <div class="sidebar">
+        <a class="brand-logo" href="index.html">
+            <img src="{{ asset('/images/logo.png') }}" alt="">
+            <span>Pay Pooler </span></a>
+        <div class="menu">
+            <ul>
+                <li>
+                    <a href="/home">
+                        <span><i class="mdi mdi-view-dashboard"></i></span>
+                        <span class="nav-text">Home</span>
+                    </a>
+                </li>
+                <li><a href="{{ route('user.order.index') }}">
+                        <span><i class="mdi mdi-repeat"></i></span>
+                        <span class="nav-text">My Orders</span>
+                    </a>
+                </li>
+                <li><a href="{{ route('user.offer.index') }}">
+                        <span><i class="mdi mdi-repeat"></i></span>
+                        <span class="nav-text">My Offers</span>
+                    </a>
+                </li>
+                <li><a href="{{ route('user.account') }}">
+                        <span><i class="mdi mdi-account"></i></span>
+                        <span class="nav-text">Account</span></a></li>
                 <li><a href="{{ route('user.account.show') }}">
                         <span><i class="mdi mdi-settings"></i></span>
                         <span class="nav-text">Setting</span>
@@ -81,9 +81,9 @@
                     </a>
                 </li>
                 <li><a href="{{ route('user.order.index') }}">
-                    <span><i class="mdi mdi-repeat"></i></span>
-                    <span class="nav-text">سفارشات من</span>
-                </a>
+                        <span><i class="mdi mdi-repeat"></i></span>
+                        <span class="nav-text">سفارشات من</span>
+                    </a>
                 </li>
                 <li><a href="{{ route('user.offer.index') }}">
                         <span><i class="mdi mdi-repeat"></i></span>
